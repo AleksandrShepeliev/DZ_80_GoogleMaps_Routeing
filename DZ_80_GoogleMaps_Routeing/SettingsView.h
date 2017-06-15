5db23b02677d31e992b001d828cbf5567dceccad
macbook pro <macbookpro@Yuriis-MacBook-Pro.local>
2017-06-15 12:34:21 +0300

    Ui created

---
+++ DZ_80_GoogleMaps_Routeing/SettingsView.h
@@ -0,0 +1,13 @@
+//
+//  SettingsView.h
+//  DZ_80_GoogleMaps_Routeing
+//
+//  Created by macbook pro on 14.06.17.
+//  Copyright © 2017 Shepeliev Aleksandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsView : UIView
+
+@end
---
+++ DZ_80_GoogleMaps_Routeing/SettingsView.m
@@ -0,0 +1,21 @@
+//
+//  SettingsView.m
+//  DZ_80_GoogleMaps_Routeing
+//
+//  Created by macbook pro on 14.06.17.
+//  Copyright © 2017 Shepeliev Aleksandr. All rights reserved.
+//
+
+#import "SettingsView.h"
+
+@implementation SettingsView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
